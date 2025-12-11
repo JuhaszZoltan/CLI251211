4500763be45363396b938c09de1aa435b19c9548
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2025-12-11 08:34:53 +0100

    Add project files.

---
+++ Pontszerzo.cs
@@ -0,0 +1,26 @@
+﻿class Pontszerzo
+{
+    public int Helyezes { get; set; }
+    public int SportolokSzama { get; set; }
+    public string Sportag { get; set; }
+    public string Versenyszam { get; set; }
+    public int OlimpiaiPont => 
+        Helyezes == 1 
+        ? 7 
+        : 7 - Helyezes;
+
+    public override string ToString() =>
+        $"\tHelyezés: {Helyezes}.\n" +
+        $"\tSportág: {Sportag}\n" +
+        $"\tVersenyszám: {Versenyszam}\n" +
+        $"\tSportolók száma: {SportolokSzama} fő";
+
+    public Pontszerzo(string[] splits)
+    {
+        Helyezes = int.Parse(splits[0]);
+        SportolokSzama = int.Parse(splits[1]);
+        Sportag = splits[2];
+        Versenyszam = splits[3];
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿const string ResourcePath = "..\\..\\..\\data\\helsinki.txt";
+const string OutputPath = "..\\..\\..\\data\\helsinki2.txt";
+
+
+List<Pontszerzo> magyarok = [];
+
+using StreamReader sr = new(ResourcePath);
+while (!sr.EndOfStream) magyarok.Add(new(sr.ReadLine().Split(' ')));
+
+Console.WriteLine($"pontszerzo helyezesek szama: {magyarok.Count} db");
+
+Console.WriteLine("megszerzett ermek szama");
+Console.WriteLine($"\tarany: {magyarok.Count(p => p.Helyezes == 1)} db");
+Console.WriteLine($"\tezust: {magyarok.Count(p => p.Helyezes == 2)} db");
+Console.WriteLine($"\tbronz: {magyarok.Count(p => p.Helyezes == 3)} db");
+
+var opsum = magyarok.Sum(p => p.OlimpiaiPont);
+Console.WriteLine($"megszerzett olimpiai pontok osszesen: {opsum} pont");
+
+int uesz = magyarok.Count(p => p.Helyezes <= 3 && p.Sportag == "uszas");
+int tesz = magyarok.Count(p => p.Helyezes <= 3 && p.Sportag == "torna");
+if (uesz == tesz) Console.WriteLine("uszas es torna sportagakban egyenlo volt az ermek szama");
+else Console.WriteLine($"{(uesz > tesz ? "uszas" : "torna")} sportagban szereztek tobb ermet");
+
+using StreamWriter sw = new(OutputPath);
+foreach (var m in magyarok)
+{
+    string san = m.Sportag.Contains("kajak") ? "kajak-kenu" : m.Sportag;
+    sw.WriteLine($"{m.Helyezes} {m.SportolokSzama} {m.OlimpiaiPont} {san} {m.Versenyszam}");
+}
+Console.WriteLine($"helsinki2.txt kesz! ({OutputPath})");
+
+var mssz = magyarok.MaxBy(p => p.SportolokSzama);
+Console.WriteLine($"legtobb erintett sportolo: \n{mssz}");
